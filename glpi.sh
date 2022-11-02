31eee69d76762cfa7731152aeea2897c9c944a3d
Felipe <felipealves1995@gmail.com>
2022-11-02 11:59:26 -0300

    Atualizando o script com pacotes que faltam

--- glpi.sh
+++ glpi.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 #Script criado por Felipe Alves
+#Antes de Executar o Script, instale o GIT na máquina
 
-echo "Atualizando o Servidor"
+
+echo "======================================"
+echo "      Atualizando o Servidor"
+echo "======================================"
 apt-get update && apt-get upgrade -y
 #yum check-update
 
-echo "Instalando o Docker"
+echo "======================================"
+echo "        Instalando o Curl e o Docker"
+echo "======================================"
+apt-get install curl -y
 curl -fsSL https://get.docker.com | sh
 #systemctl start docker
 #systemctl status docker
 
+echo "======================================"
 echo "Baixando os arquivos do Docker-Compose"
+echo "======================================"
 curl -L https://github.com/docker/compose/releases/download/1.25.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-echo "Criação de diretorio para aplicabilidade da persistencia de dados"
+echo "=========================================================================="
+echo "    Criação de diretorio para aplicabilidade da persistencia de dados"
+echo "=========================================================================="
 cd /opt 
 mkdir GLPI
 cd GLPI
